5a0bb86d06d15100be9d844f7b5163a190e94310
Patrick <pjpweaver@gmail.com>
2015-02-22 16:04:48 -0500

    finished inital layout and bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,43 +1,34 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "Home Page" do
+describe "Static pages" do
 
-    it "should have the content 'Oat Dog Club'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Oat Dog Club')
-    end
+  subject { page }
 
-    it "should have the title 'Oat Dog Club'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Oat Dog Club")
-    end
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Oat Dog Club') }
+    it { should have_title(full_title('')) }
   end
 
-  describe "Help Page" do
+  describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Oat Dog Club // Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
-  describe "About Page" do
+  describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+  end
 
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Oat Dog Club // About")
-    end
+  describe "Contact page" do
+    before { visit contact_path }
 
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
